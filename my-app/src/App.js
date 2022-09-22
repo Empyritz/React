3b25c326ed39811be2d981743577ffc8223b795a
Fabian NP <fabian.nava.pereda@gmail.com>
2022-09-22 14:49:37 -0500

    Making a clone of a landing page of airbnb with React.js and differents components to make more reusable code passing trhough props array methods like map and iterate on each elemet of an array and make cards automatically using all the mention before

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+
+
+function App() {
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key={item.id}
+        //spread operator 
+        {...item}
+
+        // destructuring
+        //  item = {item}
+
+        //assigning each element 
+        // img={item.coverImg}
+        // rating={item.stats.rating}
+        // reviewCount={item.stats.reviewCount}
+        // location={item.location}
+        // price={item.price}
+        // openSpot={item.openSpots}
+      />
+    )
+  })
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import star from "../images/star.png"
+
+export default function Card (props) {
+    let badgeText
+    if (props.openSpot === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+    return (
+    <div className='card'>
+        {badgeText && <div className="card--badge">{badgeText}</div>}
+        <img src={props.coverImg} alt="" />
+        <div className='card--stats'>
+            <img src={star} alt="" className='card--star'/>
+            <span>{props.stats.rating}</span>
+            <span className='gray'>({props.stats.reviewCount}) •</span>
+            <span className='gray'>{props.location}</span>
+        </div>
+        <p>{props.title}</p>
+        <p><span className='bold'>From ${props.price}</span> / person </p>
+    </div>
+    )
+    
+}
